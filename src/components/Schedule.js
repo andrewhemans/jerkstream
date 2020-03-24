2d7974c4e3bc1c9a463ddaf4f5633d6fbb2fe32c
andrewhemans <andrewhemans@gmail.com>
2020-03-24 14:25:07 -0700

    schedule update

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,12 +4,16 @@ const Schedule = () => (
 	<div className="">
                 <ul className="schedule-list">
                     <li className="schedule-item">
-                        <p className="date-time"> March 23, 7pm </p>
-                        <p>Cheyenne Heard</p> 
-                    </li>
-                    <li className="schedule-item">
                         <p className="date-time"> March 25, 6pm </p>
                         <p>Gremnir</p>
+                    </li>
+                    <li className="schedule-item">
+                        <p className="date-time"> March 26, 7pm </p>
+                        <p>Matt Ratt</p>
+                    </li>
+                    <li className="schedule-item">
+                        <p className="date-time"> March 27, 7pm </p>
+                        <p>Geovanie Brooks</p>
                     </li>
                     <li className="schedule-item">
                         <p className="date-time"> March 29, 7pm </p>
