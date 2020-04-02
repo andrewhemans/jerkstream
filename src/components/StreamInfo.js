12006eedd6161cc92ac6575b6456936d78126602
andrewhemans <andrewhemans@gmail.com>
2020-04-01 20:03:24 -0700

    removing info panel

--- src/components/StreamInfo.js
+++ src/components/StreamInfo.js
@@ -3,12 +3,12 @@ import React from 'react';
 const StreamInfo = () => (
 	<div className="columns is-gapless">
         <div className="column">
-            {/* <div className="info-window">
+            <div className="info-window">
                 <p className="title is-2 has-text-centered has-text-primary">Philthy Phil</p>
                 <p className="has-text-centered">Leave Phil a Tip! Venmo: @philthyphuckingphil</p>
                 <p className="has-text-centered">Find Phil on Instagram <a href="https://www.instagram.com/philthyphuckingphil/">@philthyphuckingphil</a></p>
                 <p className="has-text-centered">Check out Phil on Bandcamp <a href="https://philthyxphil.bandcamp.com/">philthyxphil.bandcamp.com/</a></p>
-            </div> */}
+            </div>
         </div>
         <div className="column is-one-third chat-window">
             <iframe 
